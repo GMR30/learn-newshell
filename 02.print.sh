0444b7b3e1024700a333b69a6e2747fe81d2350d
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-20 19:48:14 +0530

    add a valid comment

--- 02.print.sh
+++ 02.print.sh
@@ -1,2 +1,10 @@
     #for printing we have many options
     echo Hellow World
+    #to print different colors
+    #Syntax echo -e "\e[COLmMessage\e[0m"
+    echo -e "\e[31mHellow in Red color\e[0m"
+    echo -e "\e[32mHello in Green color\e[0m"
+    echo -e "\e[33mHello in yellow color\e[0m"
+    echo -e "\e[34mHello in Blue color\e[0m"
+    echo -e "\e[35mHello in Magents color\e[0m"
+    echo -e "\e[36mHello in Cyan color\e[0m"
